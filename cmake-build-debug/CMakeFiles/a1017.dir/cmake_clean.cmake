326d36481d5905fb5442307fbcc369bae40104e2
Hotown <hotown@HotowndeMacBook-Pro-2.local>
2018-07-26 13:11:13 +0800

    [Add]a1014, a1017

---
+++ cmake-build-debug/CMakeFiles/a1017.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a1017.dir/pat_code/advanced_level/a1017.cpp.o"
+  "a1017.pdb"
+  "a1017"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a1017.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pat_code/advanced_level/a1017.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Hotown on 2018/7/25.
+//
+
